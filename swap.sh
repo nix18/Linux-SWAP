284eafd45e5693dcaaae9cf55ffaf0f50c65018f
nix18 <35834172+nix18@users.noreply.github.com>
2019-07-22 14:36:30 +0800

    Update swap.sh

--- swap.sh
+++ swap.sh
@@ -39,6 +39,12 @@ then
 	echo "3" > /proc/sys/vm/drop_caches
 	swapoff -a
 	rm -f /swapfile
+	sudo fallocate -l ${swap}M $1
+	sudo chmod 600 $1
+	sudo mkswap $1
+	sudo swapon $1
+	sudo cp /etc/fstab /etc/fstab.bak
+	echo -e "/$1 none swap sw 0 0" | sudo tee -a /etc/fstab
 else
 	sudo fallocate -l ${swap}M $1
 	sudo chmod 600 $1
